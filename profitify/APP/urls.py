d40c7fcd1de793a663dd23c3f4e554ff0e032d6d
Scavenger-21 <nalbalwaru@gmail.com>
2025-11-15 01:07:01 +0530

    added few feats

--- profitify/APP/urls.py
+++ profitify/APP/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
 
     path('login/', views.login_page, name='login-page'),
     
+    path('dashboard/', views.dashboard, name='dashboard-page'),
+
+    
     # This is the API endpoint for your barcode scanner
     path('api/scan/<str:barcode>/', views.scan_product_api, name='api-scan-product'),
 ]
